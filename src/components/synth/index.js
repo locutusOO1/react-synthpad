0c3f57272565523557f04d0e0ed4f42e3e8b277c
Clark Kent with glasses <61812163+locutusOO1@users.noreply.github.com>
2020-09-29 20:01:27 -0500

    added sound

---
+++ src/components/synth/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { DuoSynth, FMSynth } from 'tone';
+import { useAppContext } from '../../context';
+import Pad from './pad';
+
+const styles = {
+    display: 'grid',
+    gridGap: 10,
+    gridTemplateColumns: 'repeat(4, 1fr)',
+}
+
+
+export default () => {
+    const { state } = useAppContext()
+
+    const synth = state.mode === 'light' ? new FMSynth() : new DuoSynth();
+    synth.toDestination();
+
+    const playSound = targetLetter => {
+        const foundNote = state.notes.find(({letter}) => letter === targetLetter);
+        if (foundNote) {
+            synth.triggerAttackRelease(foundNote.note, '8n')
+        }
+    }
+
+    return (
+        <div style={ styles }>
+            {state.notes.map(note => <Pad {...note} playSound={playSound} key={note.letter} />)}
+        </div>
+    )
+}
---
+++ src/components/synth/pad.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default ({ letter, note, color, playSound }) => {
+    const styles = {
+        width: 100,
+        height: 100,
+        borderRadius: 8,
+        background: color,
+        cursor: 'pointer',
+    }
+
+    return (
+    <button onClick={() => playSound(letter)} style={styles}>{ note }</button>
+    )
+}
